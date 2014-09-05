e2afbe3fb2ca4abc23ed23d3b3784ed39ff80f21
Josh Tummel <josh.tummel@segmint.com>
2014-09-05 15:50:42 -0400

    Time to completely revamp this out of date project...

--- bin/get_transactions.php
+++
@@ -1,5 +0,0 @@
-<?php
-	echo file_get_contents('https://blockchain.info/address/13vowvhZdbeAJwEADNPkLySsiKSEfXB6Ju?format=json');
-?>
-
-
--- js/app.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on filters, and services
-angular.module('app', ['app.filters', 'app.services', 'app.directives', 'app.controllers']);
